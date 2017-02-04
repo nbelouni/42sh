7aa63686a715430bdd6601f16b9940edfb13b6e1
Nawel BELOUNIS <nbelouni@e1r10p12.42.fr>
2017-02-04 18:04:42 +0100

    edit_visual fixd

--- src/edit_visual.c
+++ src/edit_visual.c
@@ -6,7 +6,7 @@
 /*   By: alallema <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/02/04 00:00:38 by alallema          #+#    #+#             */
-/*   Updated: 2017/02/04 16:15:54 by nbelouni         ###   ########.fr       */
+/*   Updated: 2017/02/04 18:03:42 by nbelouni         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,26 +31,19 @@ void	print_post_curs(t_buf *buf)
 
 	cursor = (g_curs.win_col * g_curs.row) + g_curs.col - PROMPT_LEN;
 	i = 0;
-	PUT2("\nprint_post_curs\n");
-	PUT2("cursor + i : ");E(cursor + i);
-	PUT2(", buf->size : ");E(buf->size);
-	PUT2(", col : ");E(g_curs.col);
-	PUT2(", row");X(' ');E(g_curs.row);X('\n');
-//	(void)buf;
-//	(void)i;
 	t_puts("cd", 1);
 	t_puts("sc", 1);
+	if (cursor < buf->size
+		&& (buf->size + PROMPT_LEN) / g_curs.win_col > 0 
+		&& (buf->size + PROMPT_LEN) % g_curs.win_col == 0)
+	{
+		t_puts("do", 1);
+		t_puts("rc", 1);
+		t_puts("up", 1);
+		t_puts("sc", 1);
+	}
 	while (buf->line[cursor + i])
 	{
-		PUT2("cursor + i : ");E(cursor + i);
-		PUT2(", buf->size : ");E(buf->size);
-		PUT2(", col : ");E(g_curs.col);
-		PUT2(", row");X(' ');E(g_curs.row);X('\n');
-//		if (g_curs.row)
-//		{
-//			t_puts("do", 1);
-//			t_puts("cr", 1);
-//		}
 		ft_putchar_fd(buf->line[((g_curs.win_col * g_curs.row) + g_curs.col - PROMPT_LEN) + i], 1);
 		i++;
 		
