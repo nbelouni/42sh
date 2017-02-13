cf9b328a2baeb2a730e5e7290ac39fe7a546fdc1
Nawel BELOUNIS <nbelouni@e1r10p12.42.fr>
2017-02-13 21:07:43 +0100

    lex.h

--- inc/lex.h
+++ inc/lex.h
@@ -6,35 +6,105 @@
 /*   By: alallema <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/02/11 12:30:14 by alallema          #+#    #+#             */
-/*   Updated: 2017/02/11 23:50:26 by alallema         ###   ########.fr       */
+/*   Updated: 2017/02/13 21:07:28 by nbelouni         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "42sh.h"
-/*
+
 typedef struct		s_tok
 {
 	int				tok;
-	int				pipefd[2];
 }					t_tok;
-*/
+/*
 typedef struct		s_cmd
 {
-	t_bool			tokken;
-	int				tok;
+	int				token;
 	char			**cmd;
-	char			**arg;
 }					s_cmd;
-
-typedef struct		s_lst;
+*/
+typedef struct		s_tree
 {
-	void			*content;
-	size_t			size;
-	struct s_list	*prev;
-	struct s_list	*next;
-}					t_lst;
+	int				token;
+	char			**cmd;
+	struct s_tree	*left;
+	struct s_tree	*right;
+}					t_tree;
 
-/*J'ai travailler sur mon 21 ..
- * travaille bien !!!
- * BISOUS
- * */
+typedef struct		s_cmd
+{
+	int				token;
+	char			*s;
+	struct s_lst	*prev;
+	struct s_lst	*next;
+}					t_cmd;
+
+enum				e_token
+{
+	NONE,
+	O_BRACKET,	// "("
+	C_BRACKET,	// ")"
+	O_BRACE, 	// "{ "
+	C_BRACE, 	// ";}"
+	DOT,		// ";"
+	PIPE,		// '|"
+	OR,			// '||"
+	AND,		// '&&"
+	START,		// "^, debut de ligne" -> utile ?
+	SL_DIR,		// "<"
+	DL_DIR,		// "<<"
+	SR_DIR,		// ">"
+	DR_DIR,		// ">>"
+	AMP,		// "&"
+	AMP_DIR,	// "&>"
+	D_QUOTE,	// "\""
+	S_QUOTE,	// "'"
+	BT_QUOTE,	// "`"
+	OBT_QUOTE,	// "$("
+	CBT_QUOTE,	// ")" -> == C_BRACKET
+	COMMENT		// "#"
+};
+
+/*
+**	return == len de la chaine a strsub -> strsub(s, i, is_*());
+*/
+int		is_group(char *s, int i);
+int		is_separator(char *s, int i);
+int		is_redirection(char *s, int i);
+int		is_btquote(char *s, int i);
+int		is_dquote(char *s, int i);
+int		is_dquote(char *s, int i);
+int		is_squote(char *s, int i);
+
+int		find_btquote_end(char *s, int i, int token);
+int		find_dquote_end(char *s, int i);
+int		find_squote_end(char *s, int i);
+int		find_group_end(char *s, int i, int token);
+int		find_next_word(char *s, int i);
+
+int		lex_buf(t_buf *buf);
+/*
+ * . pour les ouvrants/fermants, compter 2 motifs differents
+ *
+ * Renvoie une portion du buffer separee par group && separators
+ * . appelle is_group(), is_closed()
+ * int		get_complete_group(char **group);
+ *
+ * Renvoie une portion du buffer separee par group && separators
+ * . appelle is_group(), is_closed() et is_separator()
+ * char		*get_complete_cmd();
+ *
+ * renvoie le type d'une portion d'une commande (cmd || arg || redirection)
+ * complete et set *token
+ * e_type	get_args(char **token);
+ *
+ * t_cmd	*set_cmd();
+ *
+ * lst functions;
+ *
+ * Quand tout ca est fait, GROS CHECK :
+ * int		can_parse();
+ * if (TRUE)
+ * 	return (lst);
+ * return (NULL);
+*/
