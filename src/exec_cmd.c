aa5466e43c7c4183b7284a08f40a63b294eb5caa
alallema <alallema@e1r10p13.42.fr>
2017-02-25 21:50:46 +0100

    save debut exec command et README

--- src/exec_cmd.c
+++ src/exec_cmd.c
@@ -6,16 +6,29 @@
 /*   By: alallema <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/02/15 16:21:02 by alallema          #+#    #+#             */
-/*   Updated: 2017/02/15 22:55:01 by alallema         ###   ########.fr       */
+/*   Updated: 2017/02/25 18:52:59 by alallema         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "exec.h"
 
-static void		ft_waitchild(char **av, char **envp)
+static void		ft_recurs_cmd(t_tree *root, t_lst *env)
+{
+	t_tree	*node;
+
+	node = root;
+	if (!node)
+		return ;
+	if (node->left)
+		();
+	if (node->right)
+		();
+	ft_recurs_cmd();
+}
+
+static void		ft_waitchild(t_tree *root, t_lst *env)
 {
 	pid_t	pid;
-	int		i;
 	int		statval;
 
 	pid = fork();
@@ -28,5 +41,16 @@ static void		ft_waitchild(char **av, char **envp)
 		waitpid(pid, &status, 0);
 	}
 	if (pid == 0)
-		ft_exec(av, envp);
+		ft_recur_cmd(root, env);
+}
+
+int				exec_cmd(t_tree *root, t_lst *env)
+{
+	int		ret;
+	t_tree	*node;
+
+	node = root;
+	ft_waitchild(node, env);
+	ft_putstr_fd("$>", 1);
+	return (0);
 }
