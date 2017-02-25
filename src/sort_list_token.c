4827b00d5f300656f4d0d01b67bbae6ff87f0309
alallema <alallema@e1r10p13.42.fr>
2017-02-25 21:42:39 +0100

    list commentaire

--- src/sort_list_token.c
+++ src/sort_list_token.c
@@ -6,7 +6,7 @@
 /*   By: alallema <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/02/18 16:51:24 by alallema          #+#    #+#             */
-/*   Updated: 2017/02/21 19:14:02 by nbelouni         ###   ########.fr       */
+/*   Updated: 2017/02/25 19:56:42 by alallema         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,6 +23,13 @@ void	sort_list_token(t_token **list)
 	elem = *list;
 	while (elem)
 	{
+		if (elem->type == CMD && elem->n_string &&
+		elem->string->flag == NO_QUOTE && elem->string->s[0] == '#')
+		{
+			elem->prev->next = NULL;
+			ft_tokendestroy(&elem);
+			return ;
+		}
 		if (elem->type == CMD && elem->prev && (elem->prev->type == CMD
 		|| elem->prev->type == ARG))
 			elem->type = ARG;
